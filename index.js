0a54d24c5b6947286c25a129dde7baa4bcbf42ca
Jesse Evans <jesseelizabeth1@gmail.com>
2018-10-30 22:30:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const main = document.querySelector('main')
 
 // 2. Add an event listener
-function onClick() {
+function onClick(event) {
   alert('I was clicked')
 }
 
@@ -10,3 +10,10 @@ main.addEventListener('click', onClick)
 
 // 1. Get our input 
 const input = document.querySelector('input')
+
+// 2. Add an event listener
+function onKeyDown(event) {
+  
+}
+
+input.addEventListener('keydown', onKeyDown)
